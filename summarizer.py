8264a56c97f3bc766f569fe9be84a1910b7c86bf
bellamabhimanyu@gmail.com <bellamabhimanyu@gmail.com>
2023-03-30 01:59:02 -0400

    added summarizer class

--- summarizer.py
+++ summarizer.py
@@ -1,33 +1,47 @@
-
-
 from transformers import BartForConditionalGeneration, BartTokenizer, BartConfig
 # from optimum.bettertransformer import BetterTransformer
 
 import time
 
+class BartModel():
+    def __init__(self):
+        # Loading the model and tokenizer for bart-large-cnn
+        self.tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
+        self.model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
+        # gpu
+        self.model = self.model.to(0)
+        # model = BetterTransformer.transform(model)
+    
+    def get_summary(self, text):
+        inputs = self.tokenizer.batch_encode_plus([original_text],return_tensors='pt')
+        inputs.to(0)
+        summary_ids = self.model.generate(inputs['input_ids'], early_stopping=True)
 
-# Loading the model and tokenizer for bart-large-cnn
+        # Decoding and printing the summary
+        bart_summary = self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
-tokenizer=BartTokenizer.from_pretrained('facebook/bart-large-cnn')
-model=BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
-model = model.to(0)
-# model = BetterTransformer.transform(model)
+        return bart_summary
 
-original_text = 'A home, or domicile, is a space used as a permanent or semi-permanent residence for one or many humans, and sometimes various companion animals. It is a fully or semi sheltered space and can have both interior and exterior aspects to it. Homes provide sheltered spaces, for instance rooms, where domestic activity can be performed such as sleeping, preparing food, eating and hygiene as well as providing spaces for work and leisure such as remote working, studying and playing.'
+if __name__=="__main__":
+    bart_model = BartModel()
 
-# Encoding the inputs and passing them to model.generate()
-# inputs = tokenizer.batch_encode_plus([original_text],return_tensors='pt', max_length = 20, truncation=True)
+    original_text = 'A home, or domicile, is a space used as a permanent or semi-permanent residence for one or many humans, and sometimes various companion animals. It is a fully or semi sheltered space and can have both interior and exterior aspects to it. Homes provide sheltered spaces, for instance rooms, where domestic activity can be performed such as sleeping, preparing food, eating and hygiene as well as providing spaces for work and leisure such as remote working, studying and playing.'
+    print (bart_model.get_summary(original_text))
 
-for i in range(5):
-    st = time.time()
-    inputs = tokenizer.batch_encode_plus([original_text],return_tensors='pt')
-    inputs.to(0)
-    summary_ids = model.generate(inputs['input_ids'], early_stopping=True)
+    # testing
+    # Encoding the inputs and passing them to model.generate()
+    # inputs = tokenizer.batch_encode_plus([original_text],return_tensors='pt', max_length = 20, truncation=True)
 
-    # Decoding and printing the summary
-    bart_summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-    print(bart_summary)
-    print (time.time()-st)
+    # for i in range(5):
+    #     st = time.time()
+    #     inputs = tokenizer.batch_encode_plus([original_text],return_tensors='pt')
+    #     inputs.to(0)
+    #     summary_ids = model.generate(inputs['input_ids'], early_stopping=True)
+
+    #     # Decoding and printing the summary
+    #     bart_summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    #     print(bart_summary)
+    #     print (time.time()-st)
 
 
 
